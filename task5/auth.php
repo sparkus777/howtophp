82b3c6da6d88da19627d0529e88bfbf27fc13b93
sparkus777 <sparkus342@gmail.com>
2022-05-25 16:03:27 +0300

    -_-

--- task5/auth.php
+++ task5/auth.php
@@ -1,3 +1,8 @@
 <?php
 $connection = mysqli_connect('localhost', 'root', '123123');
 $select_db = mysqli_select_db( $connection, 'registration.users');
+
+
+    $query = "SELECT * FROM registration.users ";
+    $result = mysqli_query($connection, $query);
+    var_dump($_POST);
